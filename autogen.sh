aaec3393e75304ab57246b6e96af71a55f79aead
Jim Klimov <jim@jimklimov.com>
2017-03-13 12:49:17 +0100

    autogen.sh : refactor `echo "$spacer"` into slightly more readable `echo_spacer()` (may be renamed to shorten)

--- autogen.sh
+++ autogen.sh
@@ -3,37 +3,40 @@
 # Autoreconf wrapper script to ensure that the source tree is in a buildable state
 
 spacer="----------------------------------------------------------------------"
+echo_spacer() {
+        echo "$spacer"
+}
 
 quit () {
-	echo "$spacer"
+	echo_spacer
 	echo "Unable to build website"
 	exit
 }
 
 # Initialize submodules and get NUT
 echo "Getting NUT"
-echo "$spacer"
+echo_spacer
 echo "Initializing the submodules..."
 git submodule init || quit
 echo "Updating the submodules..."
 git submodule update || quit
-echo "$spacer"
+echo_spacer
 
 # Call NUT's autogen.sh to regenerate files needed by NUT's configure:
 # - scripts/augeas/nutupsconf.aug.in
 # - scripts/hal/ups-nut-device.fdi.in
 # - scripts/udev/nut-usbups.rules.in
 echo "Readying NUT"
-echo "$spacer"
+echo_spacer
 cd nut && ./autogen.sh || quit
 cd ..
-echo "$spacer"
+echo_spacer
 
 # Call autoreconf
 echo "Readying NUT Website"
-echo "$spacer"
+echo_spacer
 echo "Calling autoreconf..."
 autoreconf -i || quit
-echo "$spacer"
+echo_spacer
 
 echo "You can now safely configure and build website!"
