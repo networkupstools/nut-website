b67fdb3cde3e2bf0d1b02b712adfbb1deffccad4
Jim Klimov <jimklimov@gmail.com>
2022-04-06 00:38:43 +0000

    autogen.sh: document the "make dist-files" for NUT_HISTORIC_RELEASE

--- autogen.sh
+++ autogen.sh
@@ -275,7 +275,7 @@ fi
 
 echo "You can now safely configure and build website!"
 if [ -n "${NUT_HISTORIC_RELEASE-}" ]; then
-	echo "...with: ./configure --with-NUT_HISTORIC_RELEASE=${NUT_HISTORIC_RELEASE} && { make -k ; make ; }"
+	echo "...with: ./configure --with-NUT_HISTORIC_RELEASE=${NUT_HISTORIC_RELEASE} && { make -k dist-sig-files || make dist-files; } && { make -k ; make ; }"
 else
 	echo "...with: ./configure && { make -k -j 8 ; make ; }"
 fi
